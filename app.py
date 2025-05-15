801e6d90630b61aba7562f73fe59427e5a859ee6
Jayvien Mocallay <j.mocallay.141340.tc@umindanao.edu.ph>
2025-05-16 07:14:30 +0800

    debug for deploying

--- app.py
+++ app.py
@@ -237,4 +237,4 @@ def edit_password():
     return jsonify({"success": "Password updated successfully."})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 500)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 500)))
